b1a2dc6d1c97c41c1bf960a50e35df6619feea08
michael <xvx1138@protonmail.com>
2019-01-25 22:02:37 +0100

    redo

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+// --- Variables---
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('.btn__reset')
+
 let missed = 0;
 
-// event handler - not correct ===>>
-const overlay = document.getElementById('overlay');
+// --- 5 phrases array ---
+const phrases = [
+  'veggie burrito',
+  'black coffee',
+  'green smoothie',
+  'tofu dog',
+  'vegan donut'
+];
 
-overlay.addEventListener('click', () => {
-  overlay.style.color = "red";
+// --- event listener to “Start Game” ---
+startButton.addEventListener('click', () => {
+  const overlay = document.getElementById('overlay');
+  overlay.style.display = 'none';
 });
+
+// --- getRandomPhraseAsArray function ---
+function getRandomPhraseArray(arr) {
+  const randomItem = arr[Math.floor(Math.random()*arr.length)];
+  const newArr = [];
+//Split and Return new character array
+    for (let i = 0; i < randomItem.length; i += 1) {
+      newArr.push(randomItem[i]);
+ }
+ return newArr;
+}
+
+console.log(getRandomPhraseArray(phrases));
